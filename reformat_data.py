8c04a6cbe675f6fb0c40ea50f5b2af981d6083d6
congdc00 <dinhchicongf9@gmail.com>
2023-12-21 20:48:06 +0700

    v1.0.6 fixbug

--- reformat_data.py
+++ reformat_data.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     list_voice = glob(folder_pocess)
     os.makedirs(META_PATH, exist_ok = True)
 
-    for voice in tqdm(list_voice):
+    for voice in list_voice:
         
         voice_path = voice + "/*"
         list_log = glob(voice_path)
         new_line = []
-        for log_path in list_log:
+        for log_path in tqdm(list_log):
             
             with open(log_path, 'r') as file:
                 for line in file:
@@ -23,6 +23,7 @@ if __name__ == "__main__":
                     n_line = f"{path}|{text}"
         
         output_path = f"./data/metadata/{os.path.basename(voice)}.txt"
+
         with open(output_path, 'w', encoding='utf-8') as file:
             for string in new_line:
                 file.write(string + '\n')
